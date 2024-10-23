296c75bba864e92becad1481356e96308c52251b
NeoRise456 <neorise456@gmail.com>
2024-10-23 16:00:42 -0500

    chore: jpa auditing enabled.

--- src/main/java/pe/edu/upc/smartfinance/finzar/FinzarApplication.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/FinzarApplication.java
@@ -2,8 +2,10 @@ package pe.edu.upc.smartfinance.finzar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class FinzarApplication {
 
 	public static void main(String[] args) {
