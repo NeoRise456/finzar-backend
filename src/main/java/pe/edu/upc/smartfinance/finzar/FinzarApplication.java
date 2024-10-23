a75db896f90f94c5be33b3c7d5169e79429de118
NeoRise456 <neorise456@gmail.com>
2024-10-23 15:53:50 -0500

    chore: project creation.

---
+++ src/main/java/pe/edu/upc/smartfinance/finzar/FinzarApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.smartfinance.finzar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinzarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinzarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/smartfinance/finzar/FinzarApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.smartfinance.finzar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinzarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
