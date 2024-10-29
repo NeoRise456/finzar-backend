b2e2c9371281fe70867cc1961cafd1effee6b9de
NeoRise456 <neorise456@gmail.com>
2024-10-29 18:13:03 -0500

    feat(repositories): added wallet repository.

---
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/infrastructure/persistence/jpa/repositories/WalletRepository.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.smartfinance.finzar.wallets.infrastructure.persistence.jpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.aggregates.Wallet;
+
+import java.util.Optional;
+
+public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    boolean existsByName(String name);
+    boolean existsByNameAndUserIdIsNot( String name, Long userId);
+    Optional<Wallet> findByName(String name);
+}
