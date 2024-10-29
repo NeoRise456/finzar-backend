0c49b4e188b78a8231b7bec2c07f8eedeeb160db
cwassointt <dillpixel1@gmail.com>
2024-10-29 18:53:13 -0500

    feat: create SavingResourceFromEntityAssembler for entity to resource conversion.

---
+++ src/main/java/pe/edu/upc/smartfinance/finzar/savings/interfaces/rest/transform/SavingResourceFromEntityAssembler.java
@@ -0,0 +1,8 @@
+package pe.edu.upc.smartfinance.finzar.savings.interfaces.rest.transform;
+
+import pe.edu.upc.smartfinance.finzar.savings.domain.model.aggregates.Saving;
+import pe.edu.upc.smartfinance.finzar.savings.interfaces.rest.resources.SavingResource;
+
+public class SavingResourceFromEntityAssembler {
+
+}
