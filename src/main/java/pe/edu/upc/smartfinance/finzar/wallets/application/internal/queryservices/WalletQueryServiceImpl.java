e6d02b106aea03c52829169e197ebca2bf126a07
NeoRise456 <neorise456@gmail.com>
2024-10-30 18:46:52 -0500

    feat(query-services): implemented handle for get wallet id query.

--- src/main/java/pe/edu/upc/smartfinance/finzar/wallets/application/internal/queryservices/WalletQueryServiceImpl.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/application/internal/queryservices/WalletQueryServiceImpl.java
@@ -1,13 +1,17 @@
 package pe.edu.upc.smartfinance.finzar.wallets.application.internal.queryservices;
 
+import org.springframework.stereotype.Service;
 import pe.edu.upc.smartfinance.finzar.wallets.domain.model.aggregates.Wallet;
 import pe.edu.upc.smartfinance.finzar.wallets.domain.model.queries.GetAllWalletsQuery;
-import pe.edu.upc.smartfinance.finzar.wallets.domain.services.WallerQueryService;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.queries.GetWalletByIdQuery;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.services.WalletQueryService;
 import pe.edu.upc.smartfinance.finzar.wallets.infrastructure.persistence.jpa.repositories.WalletRepository;
 
 import java.util.List;
+import java.util.Optional;
 
-public class WalletQueryServiceImpl implements WallerQueryService {
+@Service
+public class WalletQueryServiceImpl implements WalletQueryService {
 
     private final WalletRepository walletRepository;
 
@@ -18,5 +22,10 @@ public class WalletQueryServiceImpl implements WallerQueryService {
     @Override
     public List<Wallet> handle(GetAllWalletsQuery query) {
         return walletRepository.findAll();
+    }
+
+    @Override
+    public Optional<Wallet> handle(GetWalletByIdQuery query) {
+        return this.walletRepository.findById(query.walletId());
     }
 }
