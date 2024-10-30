fb1e56533b1331bbda08caa3b48b014ea656c31e
NeoRise456 <neorise456@gmail.com>
2024-10-30 18:54:03 -0500

    feat(rest): implemented controller wallet post and get.

--- src/main/java/pe/edu/upc/smartfinance/finzar/wallets/interfaces/rest/WalletController.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/interfaces/rest/WalletController.java
@@ -1,34 +1,76 @@
 package pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.queries.GetAllWalletsQuery;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.queries.GetWalletByIdQuery;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.services.WalletQueryService;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.services.WalletCommandService;
 import pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest.resources.CreateWalletResource;
 import pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest.resources.WalletResource;
+import pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest.transform.CreateWalletCommandFromResourceAssembler;
+import pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest.transform.WalletResourceFromEntityAssembler;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @CrossOrigin(origins = "*", methods = { RequestMethod.POST, RequestMethod.GET, RequestMethod.PUT, RequestMethod.DELETE })
 @RestController
 @RequestMapping(value = "/api/v1/wallets", produces = MediaType.APPLICATION_JSON_VALUE)
 @Tag(name = "Wallets", description = "Wallets Management Endpoints")
 public class WalletController {
-    //  private final ProfileQueryService profileQueryService;
-    //  private final ProfileCommandService profileCommandService;
 
-    public WalletController(){
-        //buid services
+    private final WalletQueryService walletQueryService;
+    private final WalletCommandService walletCommandService;
+
+    public WalletController(WalletQueryService walletQueryService, WalletCommandService walletCommandService) {
+        this.walletQueryService = walletQueryService;
+        this.walletCommandService = walletCommandService;
     }
 
     @PostMapping
     public ResponseEntity<WalletResource> createWallet(@RequestBody CreateWalletResource resource){
-        return ResponseEntity.ok().body(null);
+        var createWalletCommand = CreateWalletCommandFromResourceAssembler.toCommandFromResource(resource);
+        var walletId = walletCommandService.handle(createWalletCommand);
+
+        if (walletId.equals(0L)){
+            return ResponseEntity.badRequest().build();
+        }
+
+        var getWalletByIdQuery = new GetWalletByIdQuery(walletId);
+        var optionalWallet = this.walletQueryService.handle(getWalletByIdQuery);
+
+        var walletResource = WalletResourceFromEntityAssembler.toResourceFromEntity(optionalWallet.get());
+        return new ResponseEntity<>(walletResource, HttpStatus.CREATED);
     }
 
     @GetMapping
     public ResponseEntity<List<WalletResource>> getAllWallets() {
-        return ResponseEntity.ok().body(null);
+        var getAllWalletsQuery = new GetAllWalletsQuery();
+        var wallets = this.walletQueryService.handle(getAllWalletsQuery);
+        var walletResources = wallets.stream()
+                .map(WalletResourceFromEntityAssembler::toResourceFromEntity)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.ok(walletResources);
+    }
+
+    @GetMapping("/{walletId}")
+    public ResponseEntity<WalletResource> getWalletById(@PathVariable Long walletId) {
+
+        var getWalletByIdQuery = new GetWalletByIdQuery(walletId);
+        var optionalWallet = this.walletQueryService.handle(getWalletByIdQuery);
+
+        if (optionalWallet.isEmpty()){
+            return ResponseEntity.notFound().build();
+        }
+
+        var walletResource = WalletResourceFromEntityAssembler.toResourceFromEntity(optionalWallet.get());
+
+        return ResponseEntity.ok(walletResource);
     }
 
 
