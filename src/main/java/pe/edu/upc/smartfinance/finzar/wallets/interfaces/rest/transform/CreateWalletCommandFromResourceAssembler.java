d67e82bcaaf81f602dc2102969d1203183f6369c
NeoRise456 <neorise456@gmail.com>
2024-10-30 18:52:07 -0500

    feat(transform): added create wallet command from resource assembler.

---
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/interfaces/rest/transform/CreateWalletCommandFromResourceAssembler.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest.transform;
+
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.commands.CreateWalletCommand;
+import pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest.resources.CreateWalletResource;
+
+public class CreateWalletCommandFromResourceAssembler {
+    public static CreateWalletCommand toCommandFromResource(CreateWalletResource resource) {
+        return new CreateWalletCommand(resource.userId(), resource.name(), resource.balance(), resource.totalBalance());
+    }
+}
