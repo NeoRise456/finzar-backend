40e15bdc7dc37d1dd90169a571bf23a9fcbb82d2
NeoRise456 <neorise456@gmail.com>
2024-10-30 18:44:05 -0500

    feat(transform): add  WalletResourceFromEntityAssembler.

---
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/interfaces/rest/transform/WalletResourceFromEntityAssembler.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest.transform;
+
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.aggregates.Wallet;
+import pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest.resources.WalletResource;
+
+public class WalletResourceFromEntityAssembler {
+    public static WalletResource toResourceFromEntity(Wallet entity) {
+        return new WalletResource(entity.getId(), entity.getUserId(), entity.getName(), entity.getBalance(), entity.getTotalBalance());
+    }
+}
