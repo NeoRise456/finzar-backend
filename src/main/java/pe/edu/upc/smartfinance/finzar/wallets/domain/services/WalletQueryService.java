a50618577d477dfa7a19cc1b9abf2b3f2b3e98df
NeoRise456 <neorise456@gmail.com>
2024-10-30 18:51:01 -0500

    feat(services): declared handle for get wallet id query.

--- src/main/java/pe/edu/upc/smartfinance/finzar/wallets/domain/services/WalletQueryService.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/domain/services/WalletQueryService.java
@@ -2,9 +2,13 @@ package pe.edu.upc.smartfinance.finzar.wallets.domain.services;
 
 import pe.edu.upc.smartfinance.finzar.wallets.domain.model.aggregates.Wallet;
 import pe.edu.upc.smartfinance.finzar.wallets.domain.model.queries.GetAllWalletsQuery;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.queries.GetWalletByIdQuery;
+
 
 import java.util.List;
+import java.util.Optional;
 
-public interface WallerQueryService {
+public interface WalletQueryService {
     List<Wallet> handle(GetAllWalletsQuery query);
+    Optional<Wallet> handle(GetWalletByIdQuery query);
 }
