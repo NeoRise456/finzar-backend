385194a37998b7c989a6f6f0a874ba59bba84fd6
cwassointt <dillpixel1@gmail.com>
2024-10-30 19:54:03 -0500

    feat: added new methods for Saving Resource From Entity Assembler.

--- src/main/java/pe/edu/upc/smartfinance/finzar/savings/interfaces/rest/transform/SavingResourceFromEntityAssembler.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/savings/interfaces/rest/transform/SavingResourceFromEntityAssembler.java
@@ -4,5 +4,16 @@ import pe.edu.upc.smartfinance.finzar.savings.domain.model.aggregates.Saving;
 import pe.edu.upc.smartfinance.finzar.savings.interfaces.rest.resources.SavingResource;
 
 public class SavingResourceFromEntityAssembler {
-
+    public static SavingResource toResourceFromEntity(Saving entity) {
+        return new SavingResource(
+                entity.getId(),
+                entity.getUserId().value(),
+                entity.getName(),
+                entity.getTotalGoal().value(),
+                entity.getCurrentAmount().value(),
+                entity.getCategoryId(),
+                entity.getPeriod().startDate(),
+                entity.getPeriod().endDate()
+        );
+    }
 }
