d134071cac6337eaa34573f83fae6108a1c1335c
cwassointt <dillpixel1@gmail.com>
2024-10-30 19:52:12 -0500

    fix: fixed interfaces list for Saving Query Service.

--- src/main/java/pe/edu/upc/smartfinance/finzar/savings/domain/services/SavingQueryService.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/savings/domain/services/SavingQueryService.java
@@ -1,17 +1,17 @@
 package pe.edu.upc.smartfinance.finzar.savings.domain.services;
 
+
 import pe.edu.upc.smartfinance.finzar.savings.domain.model.aggregates.Saving;
+import pe.edu.upc.smartfinance.finzar.savings.domain.model.queries.GetAllSavingsQuery;
+import pe.edu.upc.smartfinance.finzar.savings.domain.model.queries.GetSavingByIdQuery;
+import pe.edu.upc.smartfinance.finzar.savings.domain.model.queries.GetSavingByNameQuery;
 
 import java.util.List;
 import java.util.Optional;
 
+
 public interface SavingQueryService {
-    List<Saving> getAllSavings();
-    Optional<Saving> getSavingById(Long id);
-    Saving createSaving(Saving saving);
-    Saving updateSaving(Long id, Saving saving);
-    void deleteSaving(Long id);
-    boolean existsByName(String name);
-    boolean existsByNameAndIdIsNot(String name, Long id);
-    Optional<Saving> findByName(String name);
+    List<Saving> handle(GetAllSavingsQuery query);
+    Optional<Saving> handle(GetSavingByIdQuery query);
+    Optional<Saving> handle(GetSavingByNameQuery query);
 }
