cab050ea0e869e30354771403753717d7a3f9061
cwassointt <dillpixel1@gmail.com>
2024-10-30 19:48:44 -0500

    feat: created record for Current Amount.

---
+++ src/main/java/pe/edu/upc/smartfinance/finzar/savings/domain/model/valueobjects/CurrentAmount.java
@@ -0,0 +1,9 @@
+package pe.edu.upc.smartfinance.finzar.savings.domain.model.valueobjects;
+
+public record CurrentAmount(Double value) {
+    public CurrentAmount {
+        if (value == null || value < 0) {
+            throw new IllegalArgumentException("Current amount cannot be negative or null");
+        }
+    }
+}
