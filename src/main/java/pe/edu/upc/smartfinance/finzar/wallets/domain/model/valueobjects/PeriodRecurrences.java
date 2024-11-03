9d7285f00a55ad1f38dcc3b3e0b43bf1ed6ae281
NeoRise456 <neorise456@gmail.com>
2024-11-02 19:16:23 -0500

    feat(wallets): added period recurrence enum value object.

---
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/domain/model/valueobjects/PeriodRecurrences.java
@@ -0,0 +1,11 @@
+package pe.edu.upc.smartfinance.finzar.wallets.domain.model.valueobjects;
+
+public enum PeriodRecurrences {
+    DAILY,
+    WEEKLY,
+    EVERY_TWO_WEEKS,
+    MONTHLY,
+    EVERY_SIX_MONTHS,
+    YEARLY,
+    NONE,
+}
