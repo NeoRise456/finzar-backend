337bbb853cf11d7565431960145119083a8d4ad5
cwassointt <dillpixel1@gmail.com>
2024-11-02 20:42:26 -0500

    feat: added new saving query service list to handle new query.

--- src/main/java/pe/edu/upc/smartfinance/finzar/savings/domain/services/SavingQueryService.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/savings/domain/services/SavingQueryService.java
@@ -3,6 +3,7 @@ package pe.edu.upc.smartfinance.finzar.savings.domain.services;
 
 import pe.edu.upc.smartfinance.finzar.savings.domain.model.aggregates.Saving;
 import pe.edu.upc.smartfinance.finzar.savings.domain.model.queries.GetAllSavingsQuery;
+import pe.edu.upc.smartfinance.finzar.savings.domain.model.queries.GetSavingByUserIdQuery;
 import pe.edu.upc.smartfinance.finzar.savings.domain.model.queries.GetSavingByIdQuery;
 import pe.edu.upc.smartfinance.finzar.savings.domain.model.queries.GetSavingByNameQuery;
 
@@ -14,4 +15,6 @@ public interface SavingQueryService {
     List<Saving> handle(GetAllSavingsQuery query);
     Optional<Saving> handle(GetSavingByIdQuery query);
     Optional<Saving> handle(GetSavingByNameQuery query);
+    List<Saving> handle(GetSavingByUserIdQuery query);
+
 }
