5fbfc17b9bbb0ca929c795ed80b5bf8ed414e9af
cwassointt <dillpixel1@gmail.com>
2024-11-02 20:43:26 -0500

    feat: added new list to find saving by user ID.

--- src/main/java/pe/edu/upc/smartfinance/finzar/savings/infrastructure/persistence/jpa/repositories/SavingRepository.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/savings/infrastructure/persistence/jpa/repositories/SavingRepository.java
@@ -3,7 +3,9 @@ package pe.edu.upc.smartfinance.finzar.savings.infrastructure.persistence.jpa.re
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.smartfinance.finzar.savings.domain.model.aggregates.Saving;
+import pe.edu.upc.smartfinance.finzar.savings.domain.model.valueobjects.UserId;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -11,5 +13,7 @@ public interface SavingRepository extends JpaRepository<Saving, Long> {
     boolean existsByName(String name);
     boolean existsByNameAndIdIsNot(String name, Long id);
     Optional<Saving> findByName(String name);
+    List<Saving> findSavingsByUserId(UserId userId);
+
 }
 
