1f4657c7d47e2cacea4d3c2763040da5da0cdc4b
NeoRise456 <neorise456@gmail.com>
2024-11-03 13:01:38 -0500

    feat(wallets): added transaction types enum.

---
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/domain/model/valueobjects/TransactionTypes.java
@@ -0,0 +1,8 @@
+package pe.edu.upc.smartfinance.finzar.wallets.domain.model.valueobjects;
+
+public enum TransactionTypes {
+    EXPENSE,
+    EARNING,
+    INCOMING_TRANSFER,
+    OUTGOING_TRANSFER
+}
