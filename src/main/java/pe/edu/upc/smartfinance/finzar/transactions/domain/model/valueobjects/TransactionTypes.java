b6f8b251fab8a27d023d72cdd6acae1cdb4af8df
NeoRise456 <neorise456@gmail.com>
2024-11-04 01:09:55 -0500

    fix(transactions): remove unused files.

--- src/main/java/pe/edu/upc/smartfinance/finzar/transactions/domain/model/valueobjects/TransactionTypes.java
+++
@@ -1,8 +0,0 @@
-package pe.edu.upc.smartfinance.finzar.transactions.domain.model.valueobjects;
-
-public enum TransactionTypes {
-    EXPENSE,
-    EARNING,
-    INCOMING_TRANSFER,
-    OUTGOING_TRANSFER
-}
--- src/main/java/pe/edu/upc/smartfinance/finzar/wallets/interfaces/rest/resources/SimplifiedWalletResource.java
+++
@@ -1,4 +0,0 @@
-package pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest.resources;
-
-public record SimplifiedWalletResource(String name, Double balance) {
-}
