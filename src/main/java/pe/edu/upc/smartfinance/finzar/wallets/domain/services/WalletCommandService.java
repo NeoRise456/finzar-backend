3d6a417f05f34d5e38bea8d53838b2c12051f58c
NeoRise456 <neorise456@gmail.com>
2024-11-03 20:33:17 -0500

    feat(wallet): add wallet services contracts.

--- src/main/java/pe/edu/upc/smartfinance/finzar/wallets/domain/services/WalletCommandService.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/domain/services/WalletCommandService.java
@@ -1,7 +1,14 @@
 package pe.edu.upc.smartfinance.finzar.wallets.domain.services;
 
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.aggregates.Wallet;
 import pe.edu.upc.smartfinance.finzar.wallets.domain.model.commands.CreateWalletCommand;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.commands.DeleteWalletCommand;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.commands.UpdateWalletCommand;
+
+import java.util.Optional;
 
 public interface WalletCommandService {
     Long handle(CreateWalletCommand command);
+    Optional<Wallet> handle(UpdateWalletCommand command);
+    Boolean handle(DeleteWalletCommand command);
 }
--- src/main/java/pe/edu/upc/smartfinance/finzar/wallets/domain/services/WalletQueryService.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/domain/services/WalletQueryService.java
@@ -3,12 +3,13 @@ package pe.edu.upc.smartfinance.finzar.wallets.domain.services;
 import pe.edu.upc.smartfinance.finzar.wallets.domain.model.aggregates.Wallet;
 import pe.edu.upc.smartfinance.finzar.wallets.domain.model.queries.GetAllWalletsQuery;
 import pe.edu.upc.smartfinance.finzar.wallets.domain.model.queries.GetWalletByIdQuery;
-
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.queries.GetWalletsByUserIdQuery;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface WalletQueryService {
+    List<Wallet> handle(GetWalletsByUserIdQuery query);
     List<Wallet> handle(GetAllWalletsQuery query);
     Optional<Wallet> handle(GetWalletByIdQuery query);
 }
