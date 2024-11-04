f36d50f632834e588271a2b533bc9638d7b1408f
NeoRise456 <neorise456@gmail.com>
2024-11-04 00:25:03 -0500

    refactor(transaction): changed period recurrence from enum to entity.

--- src/main/java/pe/edu/upc/smartfinance/finzar/transactions/domain/model/entities/PeriodRecurrence.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/transactions/domain/model/entities/PeriodRecurrence.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
-import pe.edu.upc.smartfinance.finzar.transactions.domain.model.valueobjects.PeriodRecurrences;
 
 @Entity
 @Table(name = "period_recurrences")
@@ -20,21 +19,10 @@ public class PeriodRecurrence {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Enumerated(EnumType.STRING)
+
     @Column(length = 22)
-    private PeriodRecurrences name;
+    private String name;
 
-    public PeriodRecurrence(PeriodRecurrences name) {
-        this.name = name;
-    }
-
-    public String getStringName() {
-        return name.name();
-    }
-
-    public static PeriodRecurrence toPeriodRecurrenceFromName(String name) {
-        return new PeriodRecurrence(PeriodRecurrences.valueOf(name));
-    }
 
 
 }
