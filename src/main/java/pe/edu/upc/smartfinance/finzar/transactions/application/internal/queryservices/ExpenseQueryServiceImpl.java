a9d4d3a58a698d1962015f7ff3dc17aeb5ce03e0
NeoRise456 <neorise456@gmail.com>
2024-11-04 01:09:37 -0500

    fix(transactions): added service bean to expense query services.

--- src/main/java/pe/edu/upc/smartfinance/finzar/transactions/application/internal/queryservices/ExpenseQueryServiceImpl.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/transactions/application/internal/queryservices/ExpenseQueryServiceImpl.java
@@ -1,14 +1,16 @@
 package pe.edu.upc.smartfinance.finzar.transactions.application.internal.queryservices;
 
+import org.springframework.stereotype.Service;
 import pe.edu.upc.smartfinance.finzar.transactions.domain.model.aggregates.Expense;
-import pe.edu.upc.smartfinance.finzar.transactions.domain.model.queries.GetEarningsByWalletIdAndCategoryIdQuery;
 import pe.edu.upc.smartfinance.finzar.transactions.domain.model.queries.GetExpenseByIdQuery;
+import pe.edu.upc.smartfinance.finzar.transactions.domain.model.queries.GetExpensesByWalletIdAndCategoryIdQuery;
 import pe.edu.upc.smartfinance.finzar.transactions.domain.services.ExpenseQueryService;
 import pe.edu.upc.smartfinance.finzar.transactions.infrastructure.persistence.jpa.repositories.ExpenseRepository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ExpenseQueryServiceImpl implements ExpenseQueryService {
 
     private final ExpenseRepository expenseRepository;
@@ -23,7 +25,7 @@ public class ExpenseQueryServiceImpl implements ExpenseQueryService {
     }
 
     @Override
-    public List<Expense> handle(GetEarningsByWalletIdAndCategoryIdQuery query) {
+    public List<Expense> handle(GetExpensesByWalletIdAndCategoryIdQuery query) {
 
         var expenses = this.expenseRepository.findExpensesByWallet_IdAndCategory_Id(
                 query.walletId(), query.categoryId());
--- src/main/java/pe/edu/upc/smartfinance/finzar/transactions/domain/services/ExpenseQueryService.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/transactions/domain/services/ExpenseQueryService.java
@@ -2,13 +2,13 @@ package pe.edu.upc.smartfinance.finzar.transactions.domain.services;
 
 
 import pe.edu.upc.smartfinance.finzar.transactions.domain.model.aggregates.Expense;
-import pe.edu.upc.smartfinance.finzar.transactions.domain.model.queries.GetEarningsByWalletIdAndCategoryIdQuery;
 import pe.edu.upc.smartfinance.finzar.transactions.domain.model.queries.GetExpenseByIdQuery;
+import pe.edu.upc.smartfinance.finzar.transactions.domain.model.queries.GetExpensesByWalletIdAndCategoryIdQuery;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface ExpenseQueryService {
     Optional<Expense> handle(GetExpenseByIdQuery query);
-    List<Expense> handle(GetEarningsByWalletIdAndCategoryIdQuery query);
+    List<Expense> handle(GetExpensesByWalletIdAndCategoryIdQuery query);
 }
