73b2b01935da84d3458cf300a85a7f8105dcbf3f
NeoRise456 <neorise456@gmail.com>
2024-11-03 20:50:26 -0500

    feat(wallet): added Earning Aggregate.

---
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/domain/model/aggregates/Earning.java
@@ -0,0 +1,40 @@
+package pe.edu.upc.smartfinance.finzar.wallets.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import pe.edu.upc.smartfinance.finzar.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.entities.Category;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.entities.PeriodRecurrence;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "earnings")
+@Getter
+public class Earning extends AuditableAbstractAggregateRoot<Earning> {
+
+
+    @Min(0)
+    @NotNull
+    @Column(name = "amount")
+    private Double amount;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "wallet_id")
+    private Wallet wallet;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "period_recurrence_id")
+    private PeriodRecurrence periodRecurrence;
+
+    public Earning() {
+    }
+
+
+}
