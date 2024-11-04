b5e5ddf8f961de65de1ccbbe64c8dea8a47f4a0c
NeoRise456 <neorise456@gmail.com>
2024-11-04 00:24:51 -0500

    refactor(transaction): changed transaction type from enum to entity.

--- src/main/java/pe/edu/upc/smartfinance/finzar/transactions/domain/model/entities/TransactionType.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/transactions/domain/model/entities/TransactionType.java
@@ -5,7 +5,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.With;
-import pe.edu.upc.smartfinance.finzar.transactions.domain.model.valueobjects.PeriodRecurrences;
 
 @Entity
 @Table(name = "transaction_types")
@@ -18,21 +17,11 @@ public class TransactionType {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Enumerated(EnumType.STRING)
-    @Column(length = 22)
-    private PeriodRecurrences name;
+
+    @Column(length = 22 , nullable = false)
+    private String name;
 
 
-    public TransactionType(PeriodRecurrences name) {
-        this.name = name;
-    }
 
-    public String getStringName() {
-        return name.name();
-    }
-
-    public static TransactionType toTransactionTypeFromName(String name) {
-        return new TransactionType(PeriodRecurrences.valueOf(name));
-    }
 
 }
