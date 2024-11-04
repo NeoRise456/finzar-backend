3ee2533763713b1da3ee65ab1c73067f091e607c
NeoRise456 <neorise456@gmail.com>
2024-11-03 20:50:11 -0500

    feat(wallet): added Transaction Aggregate.

---
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/domain/model/aggregates/Transaction.java
@@ -0,0 +1,44 @@
+package pe.edu.upc.smartfinance.finzar.wallets.domain.model.aggregates;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import pe.edu.upc.smartfinance.finzar.shared.domain.model.aggregates.AuditableAbstractAggregateRoot;
+import pe.edu.upc.smartfinance.finzar.wallets.domain.model.entities.TransactionType;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name = "transactions")
+public class Transaction extends AuditableAbstractAggregateRoot<Transaction> {
+
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "wallet_id")
+    private Wallet wallet;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "transaction_type_id")
+    private TransactionType transactionType;
+
+
+    @Column(name = "note",columnDefinition = "text")
+    private String note;
+
+
+    @Min(0)
+    @NotNull
+    @Column(name = "amount")
+    private Double amount;
+
+    @NotNull
+    @NotBlank
+    @Column(name = "transaction_date")
+    private LocalDateTime transactionDate;
+
+
+
+}
