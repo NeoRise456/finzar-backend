a18d0820ee4d301cb11b82cd38fd0b67158ced4a
NeoRise456 <neorise456@gmail.com>
2024-11-03 20:33:26 -0500

    feat(wallet): add wallet resource.

--- src/main/java/pe/edu/upc/smartfinance/finzar/wallets/interfaces/rest/resources/WalletResource.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/wallets/interfaces/rest/resources/WalletResource.java
@@ -1,5 +1,9 @@
 package pe.edu.upc.smartfinance.finzar.wallets.interfaces.rest.resources;
 
-
-public record WalletResource( Long id, Long userId, String name, double balance, double totalBalance) {
+public record WalletResource(
+        Long id,
+        String name,
+        Double balance,
+        SimplifiedUserResource user
+) {
 }
