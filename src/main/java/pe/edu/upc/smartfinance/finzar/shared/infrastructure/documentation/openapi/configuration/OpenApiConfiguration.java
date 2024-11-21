38ec36fb28333a924be54f254ed5c8f9ae6ed5ab
pachieeee <sebaspacheco2003@gmail.com>
2024-11-20 20:21:56 -0500

    feat(configuration): added security scheme for open api configuration.

--- src/main/java/pe/edu/upc/smartfinance/finzar/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,9 +1,12 @@
 package pe.edu.upc.smartfinance.finzar.shared.infrastructure.documentation.openapi.configuration;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -23,6 +26,20 @@ public class OpenApiConfiguration {
                 .externalDocs(new ExternalDocumentation()
                         .description("Finzar Documentation")
                         .url("https://github.com/SmartFinance-OpenSource/Report.git"));
+
+        final String securitySchemeName = "bearerAuth";
+
+        openApi.addSecurityItem(new SecurityRequirement()
+                .addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")));
+
+        // Return OpenAPI configuration object with all settings
         return openApi;
     }
 }
