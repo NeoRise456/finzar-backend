6231038ca2602f20737cdb79dcf808d6ffb584a6
NeoRise456 <neorise456@gmail.com>
2024-11-22 02:41:53 -0500

    feat: add cors config.

--- src/main/java/pe/edu/upc/smartfinance/finzar/FinzarApplication.java
+++ src/main/java/pe/edu/upc/smartfinance/finzar/FinzarApplication.java
@@ -2,7 +2,11 @@ package pe.edu.upc.smartfinance.finzar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @EnableJpaAuditing
@@ -10,6 +14,20 @@ public class FinzarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FinzarApplication.class, args);
+	}
+
+	@Configuration
+	public static class FinzarConfiguration {
+		@Bean
+		public WebMvcConfigurer corsConfigurer(){
+			return new WebMvcConfigurer() {
+				@Override
+				public void addCorsMappings(CorsRegistry registry) {
+					registry.addMapping("/**")
+							.allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+				}
+			};
+		}
 	}
 
 }
